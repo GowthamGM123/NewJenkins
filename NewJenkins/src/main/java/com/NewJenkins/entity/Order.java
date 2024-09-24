ed78af72df2b8cdbbfbfb633e34f9a6c799fe116
GowthamGM123 <Gowtham GM@Gowtham-GM>
2024-09-24 12:08:41 +0530

    new commit

---
+++ NewJenkins/src/main/java/com/NewJenkins/entity/Order.java
@@ -0,0 +1,28 @@
+package com.NewJenkins.entity;
+
+public class Order {
+
+	private int orderId;
+	private String name;
+	private String orderType;
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOrderType() {
+		return orderType;
+	}
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+	}
+	
+	
+}
